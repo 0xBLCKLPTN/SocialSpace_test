35a112b4a994dbf922929f0af32bb77ba2ae725a
Daniil Ermolaev <blcklptn@icloud.com>
2023-05-31 14:33:49 +0500

    Initial commit

---
+++ socialspace/src/components/TheFooter.tsx
@@ -0,0 +1,9 @@
+const TheFooter = () => {
+    return (
+        <footer className="container">
+            Created by &copy;0xBLCKLPTN
+        </footer>
+    )
+}
+
+export {TheFooter};
---
+++ socialspace/src/components/TheHeader.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const TheHeader = () => {
+    return (
+        <header className="container">
+            <Link href="/">Home</Link>
+        </header>
+    )
+}
+
+export {TheHeader};
